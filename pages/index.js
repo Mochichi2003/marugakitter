794fe532d117d2e3d7bffeb47d307c07a9a99b22
Mochichi2003 <44772513+Mochichi2003@users.noreply.github.com>
2020-09-09 22:25:08 +0900

    色々修正

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,62 @@
 import Head from "next/head";
+import moment from "moment";
+import React from "react";
 import styles from "../styles/Home.module.css";
-import sass from "../styles/home.module.sass";
-import moment from 'moment'
+// import sass from "../styles/home.module.sass";
 
 function Nowtime() {
   return (
-
     <p>
-      {moment().format('YYYY/MM/DD hh:mm:ss:SSS')}
+      {moment().format("YYYY/MM/DD hh:mm:ss:SSS")}
       asdddddddddddddddddddd
     </p>
-
-  )
+  );
 }
-class Clocck extends React.Component {
-  render() {
+
+function asdfievbeosk() {
+  return moment().format("YYYY/MM/DD hh:mm:ss:SSS")
+}
+function Clocck() {
+  return (
     <div>
       <h1>Hello, world!</h1>
-      <h2>It is {moment().format('YYYY/MM/DD hh:mm:ss')}.</h2>
+      <h2>
+        Now time is
+        {
+          asdfievbeosk()
+        }
+      </h2>
     </div>
-
-  }
+  );
+  // render() {
+  //   <div>
+  //     <h1>Hello, world!</h1>
+  //     <h2>
+  //       It is
+  //       {moment().format("YYYY/MM/DD hh:mm:ss")}
+  //     </h2>
+  //   </div>;
+  // }
 }
+
+setInterval(asdfievbeosk, 10);
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
 
         <link rel="manifest" href="/favicons/site.webmanifest" />
-        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+        <link
+          rel="mask-icon"
+          href="/favicons/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
 
@@ -42,6 +66,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <Nowtime />
+        <Clocck />
       </main>
 
       <footer className={styles.footer}>
@@ -51,7 +76,6 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by
-{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
