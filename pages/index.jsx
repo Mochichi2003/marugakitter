80d335331ff7edd1b4f996a014ef4445dc10a18d
Mochichi2003 <44772513+Mochichi2003@users.noreply.github.com>
2020-09-15 12:02:04 +0900

    OGアイコンも変えた

--- pages/index.jsx
+++ pages/index.jsx
@@ -87,7 +87,7 @@ export default function Home() {
         />
         <meta
           property="og:image"
-          content="https://marugakitter.vercel.app/palettelist%20(1)-02.png"
+          content="https://marugakitter.vercel.app/favicons/縦書きったーアイコン-02.png"
         />
 
         <title>縦書きったー</title>
