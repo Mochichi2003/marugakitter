d49a85cbdada811c35734535b86fbda117933c94
Mochichi2003 <44772513+Mochichi2003@users.noreply.github.com>
2020-09-29 10:44:53 +0900

    google Consoleに登録した”

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -7,7 +7,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+      <meta name="google-site-verification" content="72NDna3JeIzeiRmbkUH735GsR2d7uyByb-2JCs_jKA4" />
         {/* <link
+
           rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
           integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
